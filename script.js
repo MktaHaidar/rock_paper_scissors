994a37febb59a011cf79df2bace827a295beb512
Mykyta Haidar <turbonet9009@gmail.com>
2022-09-07 12:06:49 +0300

    Add round scenarious for a game

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice())
+
+function round(playerSelection, computerSelection) {
+    
+    if (playerSelection.toLowerCase() == 'RoCk'.toLowerCase() && computerSelection == 'Scissors') {
+        console.log("You win! Rock beats scissors")
+    } else if (playerSelection.toLowerCase() == 'PapEr'.toLowerCase() && computerSelection == 'Rock') {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+    } else if (playerSelection.toLowerCase() == 'Scissors'.toLowerCase() && computerSelection == 'Paper') {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+    } else if (playerSelection.toLowerCase() == 'Scissors'.toLowerCase() && computerSelection == 'Scissors') {
+        console.log('Draw!')
+    } else if (playerSelection.toLowerCase() == 'PapEr'.toLowerCase() && computerSelection == 'Paper') {
+        console.log('Draw!')
+    } else if (playerSelection.toLowerCase() == 'RocK'.toLowerCase() && computerSelection == 'Rock') {
+        console.log('Draw!')
+    } else {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    }
+}
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log(round(playerSelection, computerSelection))
+
+function game()
+
+
+
